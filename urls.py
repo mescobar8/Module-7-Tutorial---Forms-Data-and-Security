764c0bbac901494d292008c2915eba689eb97cc7
mescobar8 <150753821+mescobar8@users.noreply.github.com>
2025-05-05 15:39:55 -0400

    Update urls.py

--- urls.py
+++ urls.py
@@ -1,1 +1,8 @@
-path('post/new/', views.post_new, name='post_new'),
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.post_list, name='post_list'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    path('post/new/', views.post_new, name='post_new'),
+]
