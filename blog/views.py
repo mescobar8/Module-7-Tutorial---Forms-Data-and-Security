57196e580e32a76232cf1a4249ac035a197821b7
mescobar8 <150753821+mescobar8@users.noreply.github.com>
2025-05-05 16:21:28 -0400

    Update views.py

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,7 @@
 from .forms import PostForm
 from django.shortcuts import render, redirect
 from django.utils import timezone
-from django.shortcuts import get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
 
 @login_required
@@ -48,3 +48,16 @@ def post_remove(request, pk):
     if request.method == 'POST':
         post.delete()
     return redirect('post_list')
+
+def add_comment_to_post(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = CommentForm()
+    return render(request, 'blog/add_comment_to_post.html', {'form': form})
