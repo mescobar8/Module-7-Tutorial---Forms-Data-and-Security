a4b76e9f7334e1e5ccc4ce039c7399843440bbf5
mescobar8 <150753821+mescobar8@users.noreply.github.com>
2025-05-05 16:08:30 -0400

    Update urls.py

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,9 @@
-from django.urls import path
-from . import views
+from django.urls import path, include
+from django.contrib import admin
+from django.contrib.auth import views
 
 urlpatterns = [
-    path('', views.post_list, name='post_list'),
-    path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    path('admin/', admin.site.urls),
+    path('accounts/login/', views.LoginView.as_view(), name='login'),
+    path('', include('blog.urls')),
 ]
