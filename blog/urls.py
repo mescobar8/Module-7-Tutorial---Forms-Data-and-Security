0daa7fbf8640267ea2654cea5dc841afd284fbba
mescobar8 <150753821+mescobar8@users.noreply.github.com>
2025-05-05 16:14:36 -0400

    Update urls.py

--- blog/urls.py
+++ blog/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/login/', views.LoginView.as_view(), name='login'),
     path('', include('blog.urls')),
-    path('drafts/', views.post_draft_list, name='post_draft_list')
+    path('drafts/', views.post_draft_list, name='post_draft_list'),
+    path('post/<int:pk>/publish/', views.post_publish, name='post_publish')
 ]
